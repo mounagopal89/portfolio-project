b6c099a5d9b80b217377d91d6ef85c223d3ba8ee
Mona Gopal <mounagopal89@gmail.com>
2023-01-30 13:25:16 -0800

    nav js added

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,6 +5,21 @@ function Navbar() {
   return (
     <nav className="navbar">
       <h1 className="header">Mounika Jakkani</h1>
+      <span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="25"
+          height="25"
+          fill="white"
+          className="bi bi-list"
+          viewBox="0 0 16 16"
+        >
+          <path
+            fillRule="evenodd"
+            d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"
+          />
+        </svg>
+      </span>
       <ul className="nav-links">
         <li className="nav-item">
           <Link to="/">Home</Link>

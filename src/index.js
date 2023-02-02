4a304bd4e3253b4ba005196335266f93230b8c9b
Mona Gopal <mounagopal89@gmail.com>
2023-02-02 14:26:50 -0800

    'public/index.html'

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
 // import reportWebVitals from './reportWebVitals';
 
 // Before dispatch an Action,we gonna subsribe to the store.
